f7a0ee38bfc48c8d060ff2398e99fd09901d8b7c
Jonathan Wetmore <jhcwetmore@phiretech.com>
2017-12-18 08:41:21 -0600

    Initial Commit

---
+++ SwiftFun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftFun
+//
+//  Created by Jonathan Wetmore on 12/18/17.
+//  Copyright © 2017 PHIREtech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
