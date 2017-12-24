a6d303e32a9a7b52d0a3f6b4d5b5906390f85e5b
Jonathan Wetmore <jhcw@jonathans-macbook-pro.local>
2017-12-24 12:57:42 -0600

    First Commit

--- SwiftFun/ViewController.swift
+++ SwiftFun/ViewController.swift
@@ -9,9 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var buttonCount = 0
+    @IBOutlet weak var myLabel: UILabel!
+    @IBAction func myButtonAction(_ sender: Any) {
+        buttonCount = buttonCount + 1
+        if buttonCount >= 10 {
+            view.backgroundColor = UIColor.red
+            myLabel.text = "Button count > 10"
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+   
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
